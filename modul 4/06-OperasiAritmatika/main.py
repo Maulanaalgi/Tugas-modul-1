8d20f95d0f9c1151293dbb7c3716ad07fa30a1c2
Maulanaalgi <calgi180106@gmail.com>
2024-12-04 13:19:45 +0800

    Add files via upload

---
+++ modul 4/06-OperasiAritmatika/main.py
@@ -0,0 +1,12 @@
+# operasi aritmatika
+
+a = 10
+b = 3
+
+# operasi tambah +
+hasil = a + b
+print(a, "+", "=", hasil)
+
+# operasi kurang -
+hasil = a - b
+print(a, "-", b, "=")
---
+++ modul 4/07-OperasiKomparasi/main.py
@@ -0,0 +1,30 @@
+# pehatikan
+
+"""
+    hasil; dari opersi komperasi
+    selalu bertipe data boolean
+    (true/flase)
+"""
+
+# >,<,>=,<=,==, !=, is, dan is not
+
+a = 4
+b = 2
+
+# lebih dari >
+print("===lebih dari (>)")
+hasil = a > 3 # TRUE
+print(a, ">", 3, "=", hasil)
+hasil = b > 3 # FALSE 
+print(b, ">", 3, "=", hasil)
+hasil = b > 2
+print(b, ">", 2, "=", hasil)
+
+# kurang dari <
+print("===lebih dari (<)")
+hasil = a < 3 # TRUE
+print(a, "<", 3, "=", hasil)
+hasil = b < 3 # FALSE 
+print(b, "<", 3, "=", hasil)
+hasil = b > 2
+print(b, "<", 2, "=", hasil)
